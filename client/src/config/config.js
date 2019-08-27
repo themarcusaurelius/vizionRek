8932d06cabcb8a2cfc5a3ffeb082e7e6d9a3829e
Mark Mayfield <mmayfield@panzura.com>
2019-08-27 15:57:54 -0500

    deploying

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production'
   ? 'https://vizionrekognition.herokuapp.com'
-  : 'http://localhost:8080'
+  : 'http://localhost:5000'
--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
   }); 
 };
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT, () => 
