6f8799da278a65ba8cb263fd8481b34632772c24
Mark Mayfield <mmayfield@panzura.com>
2019-08-27 17:15:23 -0500

    deploying

--- elasticsearch/connection.js
+++ elasticsearch/connection.js
@@ -2,7 +2,6 @@ var elasticsearch=require('elasticsearch');
 
 var client = new elasticsearch.Client( {  
   hosts: [
-    "https://vz7z0psl02xjragvow:uqtftbo3y50koate@es.vizion.ai:443",
     "https://vzy7vat14zfu0en1dc:k2jl2t8u98w930bx@espnap.vizion.ai:443"
   ]
 });
