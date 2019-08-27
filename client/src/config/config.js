9bf69c57f9a52d437703c8d56cbe9eddbc2ec8d6
Mark Mayfield <mmayfield@panzura.com>
2019-08-27 16:58:15 -0500

    deploying

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://vizionrekognition.herokuapp.com'
+  ? 'http://vizionrekognition.herokuapp.com'
   : 'http://localhost:5000'
--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://vizionrekognition.herokuapp.com'
+  ? 'http://vizionrekognition.herokuapp.com'
   : 'http://localhost:3000'
