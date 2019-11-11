d29877813370b8bf249e15d98ca49a08660e241f
Mark Mayfield <mmayfield@panzura.com>
2019-11-11 10:27:49 -0600

    Testing out if the new URLS will load on Heroku

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'http://vizionrekognition2.herokuapp.com'
-  : 'http://localhost:3000'
+  ? 'https://vizionrekognition2.herokuapp.com'
+  : 'http://vizionrekognition2.herokuapp.com'
