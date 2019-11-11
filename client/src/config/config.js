1c1a51bc93a804ebf5dd52ada0a722e1ebdcf3ef
Mark Mayfield <mmayfield@panzura.com>
2019-11-11 11:49:50 -0600

    desktop

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://vizionrekognition2.herokuapp.com'
+  ? 'https://vizionrekognition.herokuapp.com'
   : 'http://localhost:5000'
