1c5e245c7a0ab0a9dda450f59bd73b6332e4b67a
Mark Mayfield <mmayfield@panzura.com>
2019-11-11 10:46:40 -0600

    testing on mobile

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://vizionrekognition2.herokuapp.com'
+  ? 'http://vizionrekognition2.herokuapp.com'
   : 'http://localhost:5000'
--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://vizionrekognition2.herokuapp.com'
+  ? 'http://vizionrekognition2.herokuapp.com'
   : 'http://localhost:3000'
