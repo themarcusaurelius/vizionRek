8b82616fe98257e4e7e87c6fef27627f2f4373b8
Mark Mayfield <mmayfield@panzura.com>
2019-11-12 10:13:09 -0600

    testing out conditionals for mobile and desktop urls

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,3 +1,4 @@
 export const API_URL = process.env.NODE_ENV === 'production'
   ? 'http://vizionrekognition2.herokuapp.com'
+  && 'https://vizionrekognition2.herokuapp.com'
   : 'http://localhost:5000'
